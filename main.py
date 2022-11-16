734cc61e2e9476f1ed3186092b10cb106aa9bfc7
Beycan Gözenoğlu <beycan.gozenoglu@etiya.com>
2022-11-16 23:56:52 +0300

    pythonDay1

---
+++ main.py
@@ -0,0 +1,54 @@
+print("Merhaba ETIYA!!")
+# string = metinsel ifade
+text = "10"
+print(text)
+
+#integer = tam sayı
+number = 5
+number2 = 10
+print(number + number2)
+
+#double, float, decimal = ondalıklı sayı
+dnumber = 5.3
+print(dnumber + number)
+
+#matematiksel operatörler
+print(number +22)
+print(number - 5)
+print(number * 3)
+print(number / 5)
+print(number % 5)
+#matematiksel operatörler
+
+#boolean, bool = true veya false
+isVerified = True
+
+
+#mantıksal operatörler => Her mantıksal operatör boolean değer döner
+print(1 == 1)
+print(1 != 2)
+
+print(2 > 2)
+print(2 > 2)
+
+print(2 >= 2)
+print(2 <= 2)
+#mantıksal operatörler
+
+#string operasyonları
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("mer"))
+print(text.endswith("ya"))
+
+name = "Beycan"
+age = 28
+company = "Etiya"
+
+# Beycan 23 yaşında Etiya'da çalışıyor. 
+#print(name+" "+ age +" yaşında "+company+"'da çalışıyor")
+print(f"{name} {age} yaşında {company}'da çalışıyor.")
+
+
+
