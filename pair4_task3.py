f4a955d0bcf048a931b6578da8849ba447e96fb8
Beycan Gözenoğlu <beycan.gozenoglu@etiya.com>
2022-11-18 15:40:39 +0300

    Pair4_Task3_rev

--- pair4_task3.py
+++ pair4_task3.py
@@ -1,7 +1,12 @@
 employeesCount = 0
-while employeesCount <= 0:
-    employeesCount = int(input("Enter the number of employees: "))
-
+while True:
+    try:
+        while employeesCount <= 0:
+            employeesCount = int(input("Enter the number of employees: "))
+    except:
+        print("Incorrect entry!")
+    else:
+        break
 file = open("employees.txt", "a")
 for employee in range(employeesCount):
     employeeName = input(f"Enter the {employee+1}. employee's name: ")
@@ -10,7 +15,7 @@ for employee in range(employeesCount):
         try:
             employeeSalary = float(input(f"Enter the {employee+1}. employee's salary:"))
         except:
-            print("Hatalı Giriş Yaptınız.")
+            print("Incorrect entry!")
         else:
             break
     file.writelines(f"{employeeName.upper()} {employeeSurname.upper()} - {employeeSalary}\n")
