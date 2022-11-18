756a763f1b36c23e0f2406b854a25bd08dcaa4fd
Beycan Gözenoğlu <beycan.gozenoglu@etiya.com>
2022-11-18 09:19:19 +0300

    Pair4 Task2 HW

--- pair4_task2.py
+++ pair4_task2.py
@@ -2,15 +2,13 @@ lessonCount = 0
 while lessonCount <= 0 or lessonCount >10:
     lessonCount = int(input("Ders sayısını giriniz: "))
 
-#passedLessons = 0
-#failedLessons = 0
-
 passedLessons_rev = []
 failedLessons_rev = []
 passedLessonsExams = []
 passedLessonsFinals = []
 failedLessonsExams = []
 failedLessonsFinals = []
+count = 0
 
 for i in range(lessonCount):
     lessonExam = float(input(f"{i+1}. ders için vize notunu giriniz: "))
@@ -22,18 +20,18 @@ for i in range(lessonCount):
             print("100'den aşağı bir sayı giriniz!")
         else:
             lessonAverage = (lessonExam*.4) + (lessonFinal*.6)
+            count += 1
             if lessonAverage >= 50:
-                passedLessons_rev.append(lessonAverage)
+                passedLessons_rev.append([count,lessonAverage])
                 passedLessonsExams.append(lessonExam)
                 passedLessonsFinals.append(lessonFinal)
-                #passedLessons += 1
+                print(f"Girdiğiniz {count}.  ders sınavından , {passedLessons_rev[len(passedLessons_rev) - 1]} notu ile geçtiniz.")
             else:
-                failedLessons_rev.append(lessonAverage)
+                failedLessons_rev.append([count,lessonAverage])
                 failedLessonsExams.append(lessonExam)
                 failedLessonsFinals.append(lessonFinal)
-                #failedLessons += 1
+                print(f"Girdiğiniz {count}.  ders sınavından , {failedLessons_rev[len(failedLessons_rev) - 1]} notu ile kaldınız.")
 
-#print(f"{passedLessons} adet dersten geçtiniz. {failedLessons} adet dersten kaldınız.")
 print(f"{passedLessons_rev} ortalama notları ile {len(passedLessons_rev)} dersten geçtiniz. {failedLessons_rev} ortalama notları ile {len(failedLessons_rev)} dersten kaldınız.")
 print(f"Geçtiğiniz derslerin vize notları: {passedLessonsExams}, Geçtiğiniz derslerin final notları: {passedLessonsFinals}.\nKaldığınız derslerin vize notları: {failedLessonsExams}, Kaldığınız derslerin final notları: {failedLessonsFinals}.")
 
